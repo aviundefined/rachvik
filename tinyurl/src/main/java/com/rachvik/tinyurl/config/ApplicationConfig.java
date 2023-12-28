f1df211b525954098d53636112524cb5e2834acf
avinash <iitk.avinash.nigam@gmail.com>
2023-12-28 21:38:45 +0530

    wip

--- tinyurl/src/main/java/com/rachvik/tinyurl/config/ApplicationConfig.java
+++ tinyurl/src/main/java/com/rachvik/tinyurl/config/ApplicationConfig.java
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-@EnableCassandraRepositories(basePackages="com.rachvik.tinyurl.respository")
+@EnableCassandraRepositories(basePackages = "com.rachvik.tinyurl.respository")
 public class ApplicationConfig {}
