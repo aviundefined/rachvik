bda71e05897a1282e40398fad6b6144b70630311
avinash <iitk.avinash.nigam@gmail.com>
2023-12-29 00:55:58 +0530

    wip

--- tinyurl/src/main/java/com/rachvik/tinyurl/TinyurlApplication.java
+++ tinyurl/src/main/java/com/rachvik/tinyurl/TinyurlApplication.java
@@ -2,9 +2,7 @@ package com.rachvik.tinyurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.rachvik.tinyurl", "com.rachvik.clients"})
 @SpringBootApplication
 public class TinyurlApplication {
   public static void main(String[] args) {
