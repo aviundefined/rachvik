4f98c65a545a98768f3099496ca483623d6edafc
avinash <iitk.avinash.nigam@gmail.com>
2023-12-29 02:03:30 +0530

    wip

--- tinyurl/src/main/java/com/rachvik/tinyurl/helper/URLShortener.java
+++ tinyurl/src/main/java/com/rachvik/tinyurl/helper/URLShortener.java
@@ -1,5 +1,57 @@
 package com.rachvik.tinyurl.helper;
 
-public interface URLShortener {
-  String shortURL(final String originalURL);
+import com.rachvik.clients.IdServiceGrpcClient;
+import com.rachvik.id.UniqueIdRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class URLShortener {
+  private static final String elements =
+      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private final IdServiceGrpcClient idServiceGrpcClient;
+
+  public String getHashForOriginalUrl(final String originalUrl) {
+    val request =
+        UniqueIdRequest.newBuilder().setServiceName(idServiceGrpcClient.service().name()).build();
+    val uniqueIdResponse = idServiceGrpcClient.getStub().getUniqueId(request);
+    return base10ToBase62(uniqueIdResponse.getId());
+  }
+
+  public long getIdForHashed(final String hashed) {
+    return base62ToBase10(hashed);
+  }
+
+  private int base62ToBase10(String s) {
+    var n = 0;
+    for (var i = 0; i < s.length(); i++) {
+      n = n * 62 + convert(s.charAt(i));
+    }
+    return n;
+  }
+
+  private int convert(char c) {
+    if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'a' && c <= 'z') {
+      return c - 'a' + 10;
+    }
+    if (c >= 'A' && c <= 'Z') {
+      return c - 'A' + 36;
+    }
+    return -1;
+  }
+
+  private String base10ToBase62(long n) {
+    val sb = new StringBuilder();
+    while (n != 0) {
+      sb.insert(0, elements.charAt((int) n % 62));
+      n /= 62;
+    }
+    while (sb.length() != 7) {
+      sb.insert(0, '0');
+    }
+    return sb.toString();
+  }
 }
--- tinyurl/src/main/java/com/rachvik/tinyurl/service/TinyURLService.java
+++ tinyurl/src/main/java/com/rachvik/tinyurl/service/TinyURLService.java
@@ -1,23 +1,65 @@
 package com.rachvik.tinyurl.service;
 
+import com.rachvik.common.utils.StringUtils;
 import com.rachvik.tinyurl.OriginalURLRequest;
 import com.rachvik.tinyurl.ShortURLRequest;
 import com.rachvik.tinyurl.TinyURLResponse;
+import com.rachvik.tinyurl.entity.URLMapping;
+import com.rachvik.tinyurl.helper.URLShortener;
 import com.rachvik.tinyurl.respository.URLMappingRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TinyURLService {
-    private static final String SYSTEM_USER_ID = "SYSTEM_USER_ID";
+  private static final String SYSTEM_USER_ID = "SYSTEM_USER_ID";
+  private static final String BASE_SHORT_URL = "http://tiny.url/";
 
-    private final URLMappingRepository urlMappingRepository;
-    public TinyURLResponse getShortURL(final ShortURLRequest request) {
-        return null;
-    }
+  private final URLShortener urlShortener;
 
-    public TinyURLResponse getOriginalURL(final OriginalURLRequest request) {
-        return null;
+  private final URLMappingRepository urlMappingRepository;
+
+  public TinyURLResponse getShortURL(final ShortURLRequest request) {
+    val user = StringUtils.isBlank(request.getUserId()) ? SYSTEM_USER_ID : request.getUserId();
+    // first check if exists
+    val optionalURLMapping = urlMappingRepository.findByOriginalURL(request.getOriginalUrl());
+    if (optionalURLMapping.isPresent()) {
+      return TinyURLResponse.newBuilder()
+          .setOriginalUrl(request.getOriginalUrl())
+          .setShortUrl(BASE_SHORT_URL + optionalURLMapping.get().getShortURL())
+          .setUserId(user)
+          .build();
     }
+    val hashed = urlShortener.getHashForOriginalUrl(request.getOriginalUrl());
+    urlMappingRepository.save(
+        URLMapping.builder()
+            .shortURL(hashed)
+            .originalURL(request.getOriginalUrl())
+            .userId(user)
+            .build());
+    return TinyURLResponse.newBuilder()
+        .setOriginalUrl(request.getOriginalUrl())
+        .setShortUrl(BASE_SHORT_URL + hashed)
+        .setUserId(user)
+        .build();
+  }
+
+  public TinyURLResponse getOriginalURL(final OriginalURLRequest request) {
+    val user = StringUtils.isBlank(request.getUserId()) ? SYSTEM_USER_ID : request.getUserId();
+    val hashed = request.getShortUrl().substring(BASE_SHORT_URL.length());
+    val urlMapping =
+        urlMappingRepository
+            .findByShortURL(hashed)
+            .orElseThrow(
+                () ->
+                    new RuntimeException(
+                        "Original Url not found for shortUrl: " + request.getShortUrl()));
+    return TinyURLResponse.newBuilder()
+        .setOriginalUrl(urlMapping.getOriginalURL())
+        .setShortUrl(request.getShortUrl())
+        .setUserId(user)
+        .build();
+  }
 }
