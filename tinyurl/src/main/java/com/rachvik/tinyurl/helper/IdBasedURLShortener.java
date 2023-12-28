71fda6047c232cc362625339c06f29673d313d59
avinash <iitk.avinash.nigam@gmail.com>
2023-12-29 01:15:42 +0530

    grpc clients

--- tinyurl/src/main/java/com/rachvik/tinyurl/helper/IdBasedURLShortener.java
+++ tinyurl/src/main/java/com/rachvik/tinyurl/helper/IdBasedURLShortener.java
@@ -1,8 +1,11 @@
 package com.rachvik.tinyurl.helper;
 
+import com.rachvik.clients.IdServiceGrpcClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class IdBasedURLShortener {}
+public class IdBasedURLShortener {
+  private final IdServiceGrpcClient idServiceGrpcClient;
+}
